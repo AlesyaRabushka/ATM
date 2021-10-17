cbdacbea95801536514bc8cabfcbf61b07259d28
Alisssa <72038770+AlesyaRabushka@users.noreply.github.com>
2021-10-17 14:46:02 +0300

    Add files via upload

---
+++ Bank.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "Bank.h"
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+Bank::Bank() {
+	Bank_number = "no";
+	Bank_holder = "no";
+	bank_balance = 0;
+
+	ifstream from_acc("acc.txt");
+	if (from_acc) {
+		getline(from_acc, Bank_number);
+		getline(from_acc, Bank_holder);
+		from_acc >> bank_balance;
+
+	}
+	from_acc.close();
+}
+
+void Bank::SetAccBalance(int balance) {
+	this->bank_balance = balance;
+}
+void Bank::SetBankHolder(string holder) {
+	this->Bank_holder = holder;
+}
+void Bank::SetBankNumber(string number) {
+	this->Bank_number = number;
+}
+
+string Bank::GetAccNumber() {
+	return Bank_number;
+}
+string Bank::GetAccHolder() {
+	return Bank_holder;
+}
+int Bank::GetAccBalance() {
+	return bank_balance;
+}
