6117f93bf1172fa0ba1dd2733441c672ab9561ec
Alisssa <72038770+AlesyaRabushka@users.noreply.github.com>
2021-10-19 23:00:37 +0300

    Update Currency.h

--- Currency.h
+++ Currency.h
@@ -8,16 +8,18 @@ using namespace std;
 
 // CHANGE CURRENCY CLASS
 
-class Currency: public GiveMoney, public virtual Bankomat {
+class Currency: public GiveMoney, public virtual Bankomat, public Statement {
 private:
-	int kazah = 174, kvacha = 7, kirgiz = 35, ukrain = 11, myanma = 777;
+	double kazah = 174, kvacha = 7, kirgiz = 35, ukrain = 11, myanma = 777;
 	string kazah_t = ".00 (казахстанский тенге)", kvacha_t = ".00 (замбийская квача)", kirgiz_t = ".00 (киргизская сома)", ukrain_t = ".00 (украинская гривна)", myanma_t = ".00 (мьянманский кьят)";
 
 public:
 	// вывод на экран
 	void Print();
 	// для перемены на карточке
-	void MoneyOut(Card&, int);
+	void MoneyOut(Card&, double);
 	//выбор валюты
 	int Choice();
+	//запись в выписку
+	void ToFileFrom(Card&, double);
 };
