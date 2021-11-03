b36e535a0418e83a68be2948dd6bd035e27ff2cb
Alisssa <72038770+AlesyaRabushka@users.noreply.github.com>
2021-11-03 19:33:19 +0300

    Update Statement.cpp

--- Statement.cpp
+++ Statement.cpp
@@ -2,17 +2,37 @@
 #include "Card.h"
 #include <iostream>
 #include <fstream>
+#include <stdlib.h>
+#include <ctime>
 
 
 using namespace std;
+
 
 // выписка о пополнении средств карточки
 void Statement::ToFileIn(Card& card, double money) {
 	ofstream record("statement.txt", ios::app);
 
+	time_t now = time(0);
+	tm* t = new tm;
+	localtime_s(t, &now);
+
 	if (record) {
 		record << "\t--------------------------------" << endl;
 		record << "\tПополнение средств:" << endl;
+		record << "\tДата: ";
+		if (t->tm_mday < 10) record << "0" << t->tm_mday << ".";
+		else record << t->tm_mday << ".";
+		record << 1 + t->tm_mon << "." << 1900 + t->tm_year << endl;
+
+		record << "\tВремя: ";
+		if (t->tm_hour < 10) record << "0" << t->tm_hour << ":";
+		else record << t->tm_hour << ":";
+		if (t->tm_min < 10) record << "0" << t->tm_min << ":";
+		else record << t->tm_min << ":";
+		if (t->tm_sec < 10) record << "0" << t->tm_sec << endl;
+		else record << t->tm_sec << endl;
+
 		record << "\tНомер карты: " << card.GetNumber() << endl;
 		record << "\tСумма операции: " << money << endl;
 		record << "\tДоступные средства: " << card.GetBalance() << endl;
@@ -25,10 +45,26 @@ void Statement::ToFileIn(Card& card, double money) {
 // выписка о снятии средств с карточки
 void Statement::ToFileFrom(Card& card, double money) {
 	ofstream record("statement.txt", ios::app);
-
+	time_t now = time(0);
+	tm* t = new tm;
+	localtime_s(t, &now);
 	if (record) {
 		record << "\t--------------------------------" << endl;
 		record << "\tСнятие средств:" << endl;
+
+		record << "\tДата: ";
+		if (t->tm_mday < 10) record << "0" << t->tm_mday << ".";
+		else record << t->tm_mday << ".";
+		record << 1 + t->tm_mon << "." << 1900 + t->tm_year << endl;
+
+		record << "\tВремя: ";
+		if (t->tm_hour < 10) record << "0" << t->tm_hour << ":";
+		else record << t->tm_hour << ":";
+		if (t->tm_min < 10) record << "0" << t->tm_min << ":";
+		else record << t->tm_min << ":";
+		if (t->tm_sec < 10) record << "0" << t->tm_sec << endl;
+		else record << t->tm_sec << endl;
+
 		record << "\tНомер карты : " << card.GetNumber() << endl;
 		record << "\tСумма операции: " << money << endl;
 		record << "\tОстаток средств: " << card.GetBalance() << endl;
@@ -41,10 +77,26 @@ void Statement::ToFileFrom(Card& card, double money) {
 //выписка о пополнении счета в банке
 void Statement::ToFileIn(Bank& bank, double money) {
 	ofstream record("statement.txt", ios::app);
-
+	time_t now = time(0);
+	tm* t = new tm;
+	localtime_s(t, &now);
 	if (record) {
 		record << "\t--------------------------------" << endl;
 		record << "\tПополнение счета:" << endl;
+
+		record << "\tДата: ";
+		if (t->tm_mday < 10) record << "0" << t->tm_mday << ".";
+		else record << t->tm_mday << ".";
+		record << 1 + t->tm_mon << "." << 1900 + t->tm_year << endl;
+
+		record << "\tВремя: ";
+		if (t->tm_hour < 10) record << "0" << t->tm_hour << ":";
+		else record << t->tm_hour << ":";
+		if (t->tm_min < 10) record << "0" << t->tm_min << ":";
+		else record << t->tm_min << ":";
+		if (t->tm_sec < 10) record << "0" << t->tm_sec << endl;
+		else record << t->tm_sec << endl;
+
 		record << "\tСчет получателя: " << bank.GetNumber() << endl;
 		record << "\tСумма операции: " << money << endl;
 		record << "\tДоступные средства: " << bank.GetBalance() << endl;
@@ -68,3 +120,4 @@ void Statement::Print() {
 
 	read.close();
 }
+
