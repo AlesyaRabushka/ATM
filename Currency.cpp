053e5abc1899239157635fdfd1ebc50bf9782c64
Alisssa <72038770+AlesyaRabushka@users.noreply.github.com>
2021-11-04 15:15:22 +0300

    Update Currency.cpp

--- Currency.cpp
+++ Currency.cpp
@@ -2,18 +2,20 @@
 #include <string>
 #include <cstdlib>
 #include "Currency.h"
+#include "Exception.h"
 
 using namespace std;
 
 // вывод на экран
-void Currency::Print() {
+void Currency::Print(Singleton* log) {
 	cout << "\tКУРС ВАЛЮТ" << endl << endl;
 	cout << "\t" << kazah << kazah_t << endl;
 	cout << "\t" << kvacha << kvacha_t << endl;
 	cout << "\t" << kirgiz << kirgiz_t << endl;
 	cout << "\t" << ukrain << ukrain_t << endl;
 	cout << "\t" << myanma << myanma_t << endl;
-
+	
+	log->SingletonOperation("Вывод курса валют", 1);
 	system("pause");
 	system("cls");
 }
@@ -90,12 +92,12 @@ double Currency::Choice() {
 }
 
 // снять с карточки 
-void Currency::MoneyOut(Card& card,double money) {
+void Currency::MoneyOut(Card& card,double money, Singleton* log) {
 	card.CopyData();
 	ofstream record_("card.txt");
 	try {
 		if (money > card.GetBalance() || money < 0) {
-			throw "\tОперация не может быть выполнена! Попробуйте ещё раз позже.";
+			throw Exception("Недопустимый вид ввода", "Обмен валют");
 		}
 		else {
 
@@ -111,6 +113,7 @@ void Currency::MoneyOut(Card& card,double money) {
 			}
 
 			cout << "" << endl;
+			log->SingletonOperation("Выдыча наличных: обмен валют", 1);
 			PauseF();
 			cout << "\tЗаберите ваши деньги!" << endl << endl;
 			PauseF();
@@ -120,11 +123,28 @@ void Currency::MoneyOut(Card& card,double money) {
 			cin >> k;
 			switch (k) {
 			case 1: {
+				time_t now = time(0);
+				tm* t = new tm;
+				localtime_s(t, &now);
+
 				cout << "\tЗаберите ваш чек!" << endl << endl;
 				cout << "\t--------------------" << endl;
 				cout << "\tКод операции: 003" << endl;
+				cout << "\tДата: ";
+				if (t->tm_mday < 10) cout << "0" << t->tm_mday << ".";
+				else cout << t->tm_mday << ".";
+				cout << 1 + t->tm_mon << "." << 1900 + t->tm_year << endl;
+
+				cout << "\tВремя: ";
+				if (t->tm_hour < 10) cout << "0" << t->tm_hour << ":";
+				else cout << t->tm_hour << ":";
+				if (t->tm_min < 10) cout << "0" << t->tm_min << ":";
+				else cout << t->tm_min << ":";
+				if (t->tm_sec < 10) cout << "0" << t->tm_sec << endl;
+				else cout << t->tm_sec << endl;
 				cout << "\tСумма выдачи: " << money << endl;
 				cout << "\t--------------------" << endl;
+				log->SingletonOperation("Выдача чека", 1);
 				PauseF();
 				cout << endl;
 				break;
@@ -136,8 +156,9 @@ void Currency::MoneyOut(Card& card,double money) {
 			}
 		}
 	}
-	catch (const char* exception) {
-		cerr << exception << endl << endl;
+	catch (Exception& exception) {
+		cerr << exception.what() << endl << endl;
+		log->SingletonOperation("Выдыча наличных: обмен валют", 0);
 		ofstream record("card.txt");
 		if (record) {
 			record << card.GetNumber() << endl;
@@ -158,8 +179,26 @@ void Currency::ToFileFrom(Card& card, double money) {
 	ofstream record("statement.txt", ios::app);
 
 	if (record) {
+		time_t now = time(0);
+		tm* t = new tm;
+		localtime_s(t, &now);
+
 		record << "\t--------------------------------" << endl;
 		record << "\tСнятие средств:" << endl;
+
+		record << "Дата: ";
+		if (t->tm_mday < 10) record << "0" << t->tm_mday << ".";
+		else record << t->tm_mday << ".";
+		record << 1 + t->tm_mon << "." << 1900 + t->tm_year << endl;
+
+		record << "Время: ";
+		if (t->tm_hour < 10) record << "0" << t->tm_hour << ":";
+		else record << t->tm_hour << ":";
+		if (t->tm_min < 10) record << "0" << t->tm_min << ":";
+		else record << t->tm_min << ":";
+		if (t->tm_sec < 10) record << "0" << t->tm_sec << endl;
+		else record << t->tm_sec << endl;
+
 		record << "\tНомер карты : " << card.GetNumber() << endl;
 		record << "\tСумма операции: " << money << endl;
 		record << "\tОстаток средств: " << card.GetBalance() << endl;
